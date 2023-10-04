7e9f9f4333a4ce99ced507e8aa7478662784a4a4
MiniMaliZ <gakisomlebu@gmail.com>
2023-10-04 11:04:12 +0700

    Commit change ternary

--- Praktikum05/PemilihanBilangan27.java
+++ Praktikum05/PemilihanBilangan27.java
@@ -8,10 +8,13 @@ public class PemilihanBilangan27 {
         System.out.print("Masukkan Angka : ");
         angka = sc.nextInt();
 
-        if(angka % 2 == 0){
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        // if(angka % 2 == 0){
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // } else {
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
+
+        String result = (angka % 2 == 0) ? ("Angka " + angka + " termasuk bilangan genap"):("Angka " + angka + " termasuk bilangan ganjil");
+        System.out.println(result);
     }
 }
