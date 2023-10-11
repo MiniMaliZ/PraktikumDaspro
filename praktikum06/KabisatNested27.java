b26c57f9d987798f1ff24a5402cce779b02fee03
MiniMaliZ <gakisomlebu@gmail.com>
2023-10-11 11:12:13 +0700

    Percobaan 1 Soal 3

--- praktikum06/KabisatNested27.java
+++ praktikum06/KabisatNested27.java
@@ -12,9 +12,9 @@ public class KabisatNested27 {
         if ((tahun % 4) == 0){
             if ((tahun % 100) != 0){
                 System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
             }
-        } else {
-            System.out.println("Bukan Tahun Kabisat");
         }
     }
 }
