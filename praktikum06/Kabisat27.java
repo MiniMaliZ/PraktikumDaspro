bec2bafe2488e7950b8700f16cab6dc156355b75
MiniMaliZ <gakisomlebu@gmail.com>
2023-10-11 11:34:57 +0700

    Percobaan 1 Soal 9

---
+++ praktikum06/Kabisat27.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kabisat27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan Tahun: ");
+        tahun = sc.nextInt();
+
+        if((tahun % 4) == 0 && (tahun % 100) != 0){
+            System.out.println("Tahun Kabisat");
+        } else if ((tahun % 400) == 0){
+            System.out.println("Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
