60ffdc387fad2e07822c120bf85e0624fdfb49d2
MiniMaliZ <gakisomlebu@gmail.com>
2023-11-15 11:20:42 +0700

    feat: mengganti deklarasi int dan perbandingan loop

--- Praktikum11/Star27.java
+++ Praktikum11/Star27.java
@@ -3,11 +3,13 @@ public class Star27{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Masukkan Jumlah * : ");
+        System.out.print("Masukkan Jumlah * : ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i--) {
             System.out.println("*");
         }
+
+        sc.close();
     }
 }
