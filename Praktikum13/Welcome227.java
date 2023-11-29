e081bcdde56ebf917a42181be8502e768dced10d
MiniMaliZ <gakisomlebu@gmail.com>
2023-11-29 11:34:34 +0700

    add: new file Welcome227.java

---
+++ Praktikum13/Welcome227.java
@@ -0,0 +1,11 @@
+public class Welcome227 {
+    public static void main(String[] args) {
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("Budi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+    }
+    public static void displayWelcomeMessage(String name){
+        System.out.println("Selamat Datang di Politeknik Negeri Malang" + name);
+    }
+}
