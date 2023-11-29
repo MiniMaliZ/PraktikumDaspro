fe5d5eaa235dcaecbe0db1639aaf89773b3224e3
MiniMaliZ <gakisomlebu@gmail.com>
2023-11-29 10:55:47 +0700

    First Commit

---
+++ Praktikum13/Welcome27.java
@@ -0,0 +1,9 @@
+public class Welcome27 {
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+
+    public static void displayWelcomeMessage(){
+        System.out.println("Selamat Datang di Politeknik Negeri Malang");
+    }
+}
